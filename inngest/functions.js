9298b6910d8f0984c596fb4b1ac083e53f231323
dhruwvaviya999 <dhruwvaviya123@gmail.com>
2026-01-15 23:42:10 +0530

    feat: integrate Inngest for user sync with Clerk and update Prisma schema
    - Added Inngest client and API route for user creation, update, and deletion sync.
    - Updated Prisma schema to include new User, Product, Order, OrderItem, Rating, Address, Coupon, and Store tables.
    - Added migration files for the new database schema.
    - Updated package.json to include necessary dependencies for Inngest and Prisma.

---
+++ inngest/functions.js
@@ -0,0 +1,51 @@
+import { prisma } from "@/lib/prisma";
+import { inngest } from "./client";
+
+export const syncUserCreation = inngest.createFunction(
+  { id: "sync-user-create" },
+  { event: "clerk/user.created" },
+  async ({ event, step }) => {
+    const {data } = event;
+    await prisma.user.create({
+        data: {
+            id: data.id,
+            email: data.email_addresses[0]?.email_addresses,
+            name: `${data.first_name} ${data.last_name}`,
+            image: data.image.url
+        }
+    })
+  },
+);
+
+// Inngest function to sync user updates from Clerk to the database
+export const syncUserUpdation = inngest.createFunction(
+  { id: "sync-user-update" },
+  { event: "clerk/user.updated" },
+  async ({ event, step }) => {
+    const {data } = event;
+    await prisma.user.update({
+        where: {
+            id: data.id
+        },
+        data: {
+            email: data.email_addresses[0]?.email_addresses,
+            name: `${data.first_name} ${data.last_name}`,
+            image: data.image.url
+        }
+    })
+  },
+);
+
+// Inngest function to sync user deletions from Clerk to the database
+export const syncUserDeletion = inngest.createFunction(
+  { id: "sync-user-delete" },
+  { event: "clerk/user.deleted" },
+    async ({ event, step }) => {
+    const {data } = event;
+    await prisma.user.delete({
+        where: {
+            id: data.id
+        }
+    })
+  }
+);
